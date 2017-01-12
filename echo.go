9ae5e7f003e7bf285650a5092161ebe4a4af19af
Larry Anderson <larryboymi@hotmail.com>
2017-01-11 18:20:33 -0600

    echoing headers

--- echo.go
+++ echo.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type echo struct {
+	Headers []string
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	var request []string
+	// Loop through headers
+	for name, headers := range r.Header {
+		name = strings.ToLower(name)
+		for _, h := range headers {
+			request = append(request, fmt.Sprintf("%v: %v", name, h))
+		}
+	}
+	js, err := json.Marshal(echo{request})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(js)
+}
 
 func main() {
-  fmt.Printf("Hello, world.\n")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
